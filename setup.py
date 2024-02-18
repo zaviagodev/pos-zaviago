a38cb3766d2da16c4e55c383e363b0d76d9fa45b
Nabeel Goraya <itsgoraya@gmail.com>
2024-02-18 15:22:13 +0500

    update

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in posawesome/__init__.py
-from posawesome import __version__ as version
+# get version from __version__ variable in poszaviago/__init__.py
+from poszaviago import __version__ as version
 
 setup(
-    name="posawesome",
+    name="poszaviago",
     version=version,
     description="POS Awesome",
     author="Yousef Restom",

6ae6efc15f8d815f80c8a7615b3e450830013f9a
Nabeel Goraya <itsgoraya@gmail.com>
2024-03-18 17:22:41 +0500

    Update posapp.py

--- poszaviago/poszaviago/api/posapp.py
+++ poszaviago/poszaviago/api/posapp.py
@@ -576,9 +576,7 @@ def update_invoice(data):
     else:
         invoice_doc = frappe.get_doc(data)
         
-    if data.get("discount_amount"):
-        invoice_doc.is_cash_or_non_trade_discount = 1
-        invoice_doc.additional_discount_account = "5370-04-02 REPAIR AND MAINTENANCE - VEHICLE"
+    
         
     invoice_doc.set_missing_values()
     invoice_doc.flags.ignore_permissions = True
@@ -634,7 +632,11 @@ def update_invoice(data):
     if frappe.get_cached_value("POS Profile", invoice_doc.pos_profile, "sales_person"):
         invoice_doc.sales_name = frappe.get_cached_value("POS Profile", invoice_doc.pos_profile, "sales_person")
         
-    
+    if data.get("discount_amount"):
+        invoice_doc.is_cash_or_non_trade_discount = 1
+        invoice_doc.additional_discount_account = frappe.get_cached_value("POS Profile", invoice_doc.pos_profile, "discount_account")
+        
+        
     invoice_doc.save()
     frappe.db.commit()
     

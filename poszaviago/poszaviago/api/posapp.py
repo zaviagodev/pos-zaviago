ddcaae053a604797bbf061257a801c9e08e819bc
Nabeel Goraya <itsgoraya@gmail.com>
2024-03-18 15:51:12 +0500

    Update posapp.py

--- poszaviago/poszaviago/api/posapp.py
+++ poszaviago/poszaviago/api/posapp.py
@@ -576,6 +576,11 @@ def update_invoice(data):
     else:
         invoice_doc = frappe.get_doc(data)
         
+        
+    if data.get("discount_amount"):
+        invoice_doc.is_cash_or_non_trade_discount = 1
+        invoice_doc.additional_discount_account = frappe.get_cached_value("POS Profile", invoice_doc.pos_profile, "custom_discount_account")
+        
 
     invoice_doc.set_missing_values()
     invoice_doc.flags.ignore_permissions = True
